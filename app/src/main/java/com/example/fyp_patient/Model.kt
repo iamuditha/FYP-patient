34dd25a4d9f6809871e34786b8c9654b62e19c1b
uditha-anuranjana <udithaxy@gmail.com>
2020-07-07 12:49:54 +0530

    added initial recycle view

---
+++ app/src/main/java/com/example/fyp_patient/Model.kt
@@ -0,0 +1,10 @@
+package com.example.fyp_patient
+
+import java.util.*
+
+data class Photographer(
+    val id : Int,
+    val time : Date,
+    val temperature : Float,
+    val description : String
+)
